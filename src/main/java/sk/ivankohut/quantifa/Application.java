61cc4342129bc784cc85c44b1559117b615fe310
Ivan Kohut <Ivan.Kohut@ibm.com>
2020-12-17 00:06:32 +0100

    #25 Common exception handler

--- src/main/java/sk/ivankohut/quantifa/Application.java
+++ src/main/java/sk/ivankohut/quantifa/Application.java
@@ -15,10 +15,21 @@ public class Application {
         return price.price().map(Object::toString).orElse("");
     }
 
-    @SuppressWarnings({"PMD.SystemPrintln", "java:S106"})
+    @SuppressWarnings({ "PMD.SystemPrintln", "java:S106", "PMD.AvoidPrintStackTrace", "java:S4507", "PMD.AvoidCatchingGenericException" })
     public static void main(String[] args) {
+        int status;
         try (var twsApi = new TwsApiController("localhost", 7496, new ApiController(new TwsConnectionHandler()), 500)) {
             System.out.println(new Application(twsApi).toString());
+            status = 0;
+        } catch (ApplicationException e) {
+            System.out.println("Error: " + e.getMessage());
+            status = 1;
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error:");
+            e.printStackTrace();
+            status = 2;
         }
+        // terminate the JVM, not just the current thread
+        System.exit(status);
     }
 }
