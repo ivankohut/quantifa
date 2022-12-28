9c8cdf320e9aff2c15f2750df1d1d2c0eab4d330
Ivan Kohut <ivan.kohut@ibm.com>
2022-12-28 11:01:08 +0100

    #93 [ref] Migration to cactoos 0.55.0

--- src/main/java/sk/ivankohut/quantifa/AverageOfTheMostRecent.java
+++ src/main/java/sk/ivankohut/quantifa/AverageOfTheMostRecent.java
@@ -1,26 +1,28 @@
 package sk.ivankohut.quantifa;
 
+import org.cactoos.Scalar;
 import org.cactoos.iterable.Mapped;
 import org.cactoos.iterable.Sorted;
 import org.cactoos.iterable.TailOf;
-import org.cactoos.scalar.AvgOf;
+import org.cactoos.number.AvgOf;
 import sk.ivankohut.quantifa.decimal.DecimalOf;
 
 import java.util.Comparator;
 
 public class AverageOfTheMostRecent extends DecimalOf {
 
-    // necessary because the parameter of the ctor of AvgOf is defined as Iterable<Scalar<Number>> instead of Iterable<Scalar<? extends Number>>
-    @SuppressWarnings({ "unchecked", "rawtypes" })
     public AverageOfTheMostRecent(Iterable<? extends ReportedAmount> amounts, int numberOfItems) {
         super(new AvgOf(
-                (Iterable) new TailOf<>(
-                        numberOfItems,
-                        new Mapped<>(
-                                ReportedAmount::value,
-                                new Sorted<>(
-                                        Comparator.comparing(ReportedAmount::date),
-                                        amounts
+                new Mapped<>(
+                        Scalar::value,
+                        new TailOf<>(
+                                numberOfItems,
+                                new Mapped<>(
+                                        ReportedAmount::value,
+                                        new Sorted<>(
+                                                Comparator.comparing(ReportedAmount::date),
+                                                amounts
+                                        )
                                 )
                         )
                 )
