515f071020e0d0654970cfea3ebdf8424e932ce3
Ivan Kohut <ivan.kohut@ibm.com>
2022-12-28 20:30:00 +0100

    #93 [ref] Migration from PMD 6.47 to 6.52

--- src/acceptanceTest/java/fixture/CurrentAssetsRatiosFixture.java
+++ src/acceptanceTest/java/fixture/CurrentAssetsRatiosFixture.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@SuppressWarnings("PMD.ImmutableField")
 public class CurrentAssetsRatiosFixture {
 
     private Optional<BigDecimal> currentAssets;
